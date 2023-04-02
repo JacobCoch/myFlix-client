30449aed638460dafd9f39532e7d5155ebb9c6c1
JarJarDinks <Cochranejacob22@gmail.com>
2023-04-01 20:40:53 -0700

    app now works

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,13 @@
 import { createRoot } from 'react-dom/client';
+import { MainView } from './components/MainView/main-view.jsx';
 
 import './index.scss';
 
-const MyFlixApplication = () => {
-  return (
-    <div className="my-flix">
-      <div>Good morning</div>
-    </div>
-  );
+const App = () => {
+  return <MainView />;
 };
 
 const container = document.querySelector('#root'); // #root is the id of the div in index.html
 const root = createRoot(container); // createRoot is a new API in React 18
 
-root.render(<MyFlixApplication />); // render the application
+root.render(<App />); // render the application
