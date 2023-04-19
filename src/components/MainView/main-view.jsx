c1f37a6fb57e0f3aef2cf7326be4f77077818542
JarJarDinks <Cochranejacob22@gmail.com>
2023-04-18 17:01:08 -0700

    add routing from react-router

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -3,15 +3,23 @@ import { MovieView } from '../MovieView/movie-view.jsx';
 import { MovieCard } from '../MovieCard/movie-card.jsx';
 import { LoginView } from '../LoginView/login-view.jsx';
 import { SignupView } from '../SignupView/signup-view.jsx';
+import { NavBar } from '../NavBar/navbar.jsx';
+import { ProfileView } from '../ProfileView/profile-view.jsx';
+
+import { Container } from 'react-bootstrap';
 import { Row, Col } from 'react-bootstrap';
+import { Routes, BrowserRouter, Route, Navigate } from 'react-router-dom';
+
+//! WIP, movieview not working, need to fix
+//! WIP, cant sign up
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));
   const storedToken = localStorage.getItem('token');
-  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [user, setUser] = useState(storedUser ? storedUser : null); // if there is a user in local storage, set user to that user, else set user to null
   const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  const searchString = localStorage.getItem('searchString');
 
   useEffect(() => {
     if (!token) {
@@ -49,42 +57,94 @@ export const MainView = () => {
           Featured: movie.Featured,
         }));
         setMovies(movies);
+        localStorage.setItem('movies', JSON.stringify(movies));
       })
       .catch((err) => {
         console.error('Error fetching movies: ', err);
+        console.log(movies);
       });
   }, [token]);
 
   return (
-    <Row className="justify-content-md-center">
-      {!user ? ( // If user is not logged in, show login view
-        <Col md={5}>
-          <LoginView onLoggedIn={(user) => setUser(user)} />
-          or
-          <SignupView />
-        </Col>
-      ) : selectedMovie ? ( // If a movie is selected, show the movie view
-        <Col md={8}>
-          <MovieView
-            movie={selectedMovie}
-            onBackClick={() => setSelectedMovie(null)}
+    <BrowserRouter>
+      <NavBar
+        user={user}
+        onLoggedOut={() => {
+          localStorage.removeItem('user');
+          localStorage.removeItem('token');
+          setUser(null);
+          setToken(null);
+        }}
+      />
+
+      <Container>
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              <>{user.Username ? <Navigate to="/" /> : <SignupView />}</> // if user is logged in, redirect to home page, else show signup
+            }
           />
-        </Col>
-      ) : movies.length === 0 ? ( // If there are no movies, show a message
-        <div>The list is empty!</div>
-      ) : (
-        // Otherwise, show the list of movies
-        <>
-          {movies.map((movie) => (
-            <Col className="mb-4" key={movie.id} md={3}>
-              <MovieCard
-                movie={movie}
-                onMovieClick={(movie) => setSelectedMovie(movie)}
-              />
-            </Col>
-          ))}
-        </>
-      )}
-    </Row>
+
+          <Route
+            path="/login"
+            element={<>{user.Username ? <Navigate to="/" /> : <LoginView />}</>} // if user is logged in, redirect to home page, else show login
+          />
+
+          <Route
+            path="/movies/:movieId"
+            element={
+              <>
+                {!user.Username ? ( // if user is not logged in, redirect to login page
+                  <Navigate to="/login" />
+                ) : movies.length === 0 ? (
+                  <Col>Loading ...</Col>
+                ) : (
+                  <Row className="justify-content-md-center py-5">
+                    <Col md={8} className="mb-5">
+                      <MovieView />
+                    </Col>
+                  </Row>
+                )}
+              </>
+            }
+          />
+
+          <Route
+            path="/"
+            element={
+              <>
+                {!user.Username ? (
+                  <Navigate to="/login" />
+                ) : movies.length === 0 ? (
+                  <Col>Loading ...</Col>
+                ) : (
+                  <Row className="justify-content-md-center py-5">
+                    {searchString
+                      ? movies
+                          .filter((movie) =>
+                            movie.Title.toLowerCase().includes(searchString)
+                          )
+                          .map((movie) => (
+                            <MovieCard movie={movie} key={movie._id} />
+                          ))
+                      : movies.map((movie) => (
+                          <MovieCard movie={movie} key={movie._id} />
+                        ))}
+                  </Row>
+                )}
+              </>
+            }
+          />
+
+          <Route
+            path="/profile"
+            element={
+              <>{!user.username ? <ProfileView /> : <Navigate to="/login" />}</>
+            }
+          />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 };
