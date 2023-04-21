be7095dbb86bc12c381e0c6f0a8fd1944fdc6de1
JarJarDinks <Cochranejacob22@gmail.com>
2023-04-21 16:50:42 -0700

    add className

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export const MovieCard = ({ movie }) => {
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img variant="top" src={movie.ImagePath} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
