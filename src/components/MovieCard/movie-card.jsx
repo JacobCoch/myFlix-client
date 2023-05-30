67564aecfbfebdfca48d8da66e4964d628989269
JarJarDinks <Cochranejacob22@gmail.com>
2023-05-29 20:20:51 -0700

    add id and styles

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import { Card, Button, Row, Col } from 'react-bootstrap';
+import { FavoriteIcon } from '../FavoriteIcon/favorite-icon';
 
 export const MovieCard = ({ movie }) => {
   return (
-    <Card className="movie-cards h-100">
+    <Card className="h-100" id="card-container">
       <Row className="h-50">
         <Col className="h-100 text-center mt-3">
           <Card.Img
@@ -17,19 +18,19 @@ export const MovieCard = ({ movie }) => {
         </Col>
       </Row>
 
-      <Card.Body className="d-flex flex-column">
+      <Card.Body className="d-flex flex-column" id="card-text">
         <Card.Title className="mt-2">{movie.Title}</Card.Title>
         <Card.Text className="mt-3">{movie.Description}</Card.Text>
         <Row className="d-flex flex-row justify-content-between align-items-baseline mt-auto">
-          {/* <Col className="text-start">
+          <Col className="text-start">
             <FavoriteIcon movie={movie} />
-          </Col> */}
+          </Col>
           <Col className="text-end">
             <Link to={`/movies/${movie.Title}`}>
               {' '}
               <Button
                 className="mt-auto"
-                bg="#DCB482"
+                id="card-button"
                 variant="secondary"
                 size="sm">
                 Details
