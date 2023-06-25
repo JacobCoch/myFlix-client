ef2980f930d8155ddec1a33ed80fc8bb3cf397f7
JarJarDinks <Cochranejacob22@gmail.com>
2023-06-24 22:33:36 -0700

    btn fixes and movieview styles

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -26,43 +26,45 @@ export const MovieView = () => {
         <Col>The list is empty</Col>
       ) : (
         <>
-          <Row className="d-flex flex-row-reverse p-3" id="movie-details-view">
-            <Col md={5} className="text-center text-md-end">
+          <Row
+            className='movieview-container d-flex flex-row-reverse p-3'
+            id='movie-details-view'>
+            <Col md={5} className='text-center text-md-end'>
               <img
                 src={movie.ImagePath}
                 alt={`Poster for ${movie.Title}`}
-                className="img-fluid h-100 w-auto movie-view-img"
+                className='img-fluid h-100 w-auto movie-view-img'
               />
             </Col>
-            <Col md={7} className="d-flex flex-column">
-              <Row className="d-flex flex-row  justify-content-between">
-                <Col md={9} className="d-flex flex-column">
-                  <h3 className="my-0">
+            <Col md={7} className='d-flex flex-column'>
+              <Row className='d-flex flex-row  justify-content-between'>
+                <Col md={9} className='d-flex flex-column'>
+                  <h3 className='my-0'>
                     <span>Title: </span>
                     <span>{movie.Title}</span>
                   </h3>
-                  <h5 className="mt-1 text-left">
+                  <h5 className='mt-1 text-left'>
                     <span>Director: </span>
                     <span>{movie.Director.Name}</span>
                   </h5>
                 </Col>
 
-                <Col md={3} className="align-self-end mb-2 text-end">
+                <Col md={3} className='align-self-end mb-2 text-end'>
                   <span>Genre: </span>
-                  <span className="fw-bolder">{movie.Genre.Name}</span>
+                  <span className='fw-bolder'>{movie.Genre.Name}</span>
                 </Col>
               </Row>
-              <div className="mt-md-5 mb-4">
-                <div className=" mb-2">Description:</div>
+              <div className='mt-md-5 mb-4'>
+                <div className=' mb-2'>Description:</div>
                 <span>{movie.Description}</span>
               </div>
-              <Row className="d-flex flex-row justify-content-between mt-auto mb-md-4">
-                <Col className="text-start">
+              <Row className='d-flex flex-row justify-content-between mt-auto mb-md-4'>
+                <Col className='text-start'>
                   <FavoriteIcon movie={movie} />
                 </Col>
-                <Col className="text-end">
+                <Col className='text-end'>
                   <Link to={`/`}>
-                    <Button variant="secondary" size="lg" id="card-button">
+                    <Button variant='secondary' size='lg' id='card-button'>
                       Back
                     </Button>
                   </Link>
@@ -71,11 +73,11 @@ export const MovieView = () => {
             </Col>
           </Row>
           <Row>
-            <h2 className="mt-0">Similar movies</h2>
+            <h2 className='mt-0'>Similar movies</h2>
             <hr />
             {similarMovies.map((movie) => (
               <Col
-                className="mb-5"
+                className='mb-5'
                 key={movie._id}
                 xs={12}
                 sm={6}
